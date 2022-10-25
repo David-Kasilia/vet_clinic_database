09dd99898df709a85ad090d7628acb66713883a1
David-Kasilia <davidkasilia001@gmail.com>
2022-10-25 11:23:36 +0300

    Create invoices_items

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -32,3 +32,18 @@ FOREIGN KEY(medical_history_id)
 REFERENCES medical_histories (id)
 ON DELETE CASCADE
 );
+
+CREATE TABLE invoice_items (
+id INT GENERATED ALWAYS AS IDENTITY,
+unit_ptice DECIMAL NOT NULL,
+quantity INT NOT NULL,
+total_price DECIMAL NOT NULL,
+invoice_id INT NOT NULL,
+treatment_id INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (invoice_id)
+REFERENCES invoices (id)
+ON DELETE CASCADE,
+FOREIGN KEY(treatment_id)
+REFERENCES treatments (id)
+ON DELETE CASCADE );
