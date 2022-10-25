3813169716c9139e00ff943a96dbc7a7bb95d4b0
David-Kasilia <davidkasilia001@gmail.com>
2022-10-25 11:21:27 +0300

    Create invoices table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -20,3 +20,15 @@ id INT GENERATED ALWAYS AS IDENTITY,
 type VARCHAR(250) NOT NULL,
 name VARCHAR(250) NOT NULL,
 PRIMARY KEY(id) );
+
+CREATE TABLE invoices (
+id INT GENERATED ALWAYS AS IDENTITY,
+total_amount DECIMAL NOT NULL,
+generated_at TIMESTAMP NOT NULL,
+payed_at TIMESTAMP NOT NULL,
+medical_history_id INT NOT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY(medical_history_id)
+REFERENCES medical_histories (id)
+ON DELETE CASCADE
+);
